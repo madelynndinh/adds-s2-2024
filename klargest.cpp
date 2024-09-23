5631e4c9a1fb825a82a93a9b8fc8efa5bed9370d
madelynndinh <madelynndinh@gmail.com>
2024-09-23 17:01:20 +0930

    modify klargest

--- klargest.cpp
+++ klargest.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 int kth_largest(vector<int> values, int k)
 {
-std::priority_queue<int> maxHeap;
+std::priority_queue<int,vector<int>,greater<int>> maxHeap;
 for (int i = 0; i < values.size(); i++)
 {
     maxHeap.push(values[i]);
-}
-for (size_t i = 0; i < k; i++)
+if (maxHeap.size()>k)
 {
+
     maxHeap.pop();
 }
 return maxHeap.top();
